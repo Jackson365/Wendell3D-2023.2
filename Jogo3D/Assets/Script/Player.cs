5dda9c9b89deb7e93d829e0d439cf3001dd03cc5
Jackson Honório Cassiano <jackson.cassiano@academico.ifrn.edu.br>
2023-09-22 14:23:54 -0300

    Ajuste Na Camera

--- Jogo3D/Assets/Script/Player.cs
+++ Jogo3D/Assets/Script/Player.cs
@@ -7,6 +7,10 @@ public class Player : MonoBehaviour
     public float speed;
     private CharacterController controller;
 
+    private Transform cam;
+    private Vector3 moveDirection;
+    public float gravity;
+
     public float smoothRotTime;
     private float turnSmoothVelocity;
     
@@ -14,22 +18,49 @@ public class Player : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        cam = Camera.main.transform;
     }
     
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        Move();
+    }
 
+    private void Move()
+    {
+        if(controller.isGrounded)
+        {
+        //pega a entrada na horizontal (Tecla direita e esquerda)
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        
+        //pega a entrada na vetical (Tecla cima e baixo)
+        float vertical = Input.GetAxisRaw("Vertical");
+        
+        //varialvel local que armazena o valor do eixo horizontal e vertical
         Vector3 direction = new Vector3(horizontal, 0f, vertical);
 
-        if (direction.magnitude > 0)
-        {
-            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        //verifica se o personagem está se movimentando (se for > 0)
+            if (direction.magnitude > 0)
+            {
+            //Armazena a rotação e o angulo da camera
+            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            //Armazena a rotação mais suave
             float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
+
+            //rotaciona o personagem
             transform.rotation = Quaternion.Euler(0f,smoothAngle,0f);
-            controller.Move(direction * speed * Time.deltaTime); 
+            
+            //armazena a direção
+            moveDirection = Quaternion.Euler(0f, angle,0f) * Vector3.forward;
+
+            //move o personagem
+            //controller.Move(moveDirection * speed * Time.deltaTime); 
+            }
         }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * speed * Time.deltaTime);
     }
 }
